b7d613cef6c95904ab8ce321c17d2172beabdeb0
fessenko-tr <fessenko.tr@gmail.com>
2021-11-11 22:48:14 +0200

    добавляет первое задание и исходники

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const galleryContainerRef = document.querySelector('.gallery');
+
+addItemsToGallery(galleryItems, galleryContainerRef);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createPicMarkup({ preview, original, description }) {
+  return `<a class="gallery__item" href="${original}">
+<img class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"/>
+</a>`;
+}
+
+function addItemsToGallery(pics, gallery) {
+  gallery.innerHTML = pics.map(createPicMarkup).join('');
+}

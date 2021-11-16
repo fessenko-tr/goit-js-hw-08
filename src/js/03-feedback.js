bdecb6a35f1673273e60d05442d998fb406591bc
fessenko-tr <fessenko.tr@gmail.com>
2021-11-16 03:54:16 +0200

    исправления третьего задания

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,26 @@
 import { saveToStorage, loadFromStorage } from './localStorageFuncs';
 import lodash from 'lodash';
 
+const refs = {
+  formReff: document.querySelector('form'),
+  inputReff: document.querySelector('input'),
+  textareaReff: document.querySelector('textarea'),
+};
+
 const CURRENT_FORM_VALUE = 'feedback-form-state';
-const formReff = document.querySelector('form');
-const inputReff = formReff.querySelector('input');
-const textareaReff = formReff.querySelector('textarea');
 const throttle = lodash._.throttle;
+const savedFormData = loadFromStorage(CURRENT_FORM_VALUE);
 let email;
 let text;
 
-if (localStorage.getItem(CURRENT_FORM_VALUE)) {
+if (savedFormData) {
   const { email: email = '', text: text = '' } = loadFromStorage(CURRENT_FORM_VALUE);
-  inputReff.value = email;
-  textareaReff.innerText = text;
+  refs.inputReff.value = email;
+  refs.textareaReff.value = text;
 }
 
-formReff.addEventListener('input', throttle(saveFormData, 500));
-formReff.addEventListener('click', onBtnClick);
+refs.formReff.addEventListener('input', throttle(saveFormData, 500));
+refs.formReff.addEventListener('click', onBtnClick);
 
 function saveFormData(e) {
   const clickedTargetName = e.target.name;
@@ -32,8 +36,8 @@ function onBtnClick(e) {
     return;
   }
   e.preventDefault();
-  inputReff.value = '';
-  textareaReff.value = '';
-
+  refs.inputReff.value = '';
+  refs.textareaReff.value = '';
+  console.log(loadFromStorage(CURRENT_FORM_VALUE));
   localStorage.removeItem(CURRENT_FORM_VALUE);
 }
